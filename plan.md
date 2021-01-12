a43c0234bb2c12241d3cf94785a9e1ccfdfb8a21
Davi Lucas <davilucasdlcreator@gmail.com>
2021-01-12 20:47:55 -0300

    Create room id added

--- plan.md
+++ plan.md
@@ -1,8 +1,8 @@
 # Plan Of Action
 
-- Initialize our NodeJS Project
-- Initialize our first view
-- Create a room id
+- Initialize our NodeJS Project DONE
+- Initialize our first view DONE
+- Create a room id DONE
 - Add the ability to view our own Video
 - Add the ability to allow others to stream their videos
 - Add styling
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express()
 const server = require('http').Server(app)
+const { v4:uuidv4   } = require('uuid')
+
 const PORT = process.env.PORT || 3030
 
+app.set('view engine','ejs')
+
 app.get('/',( req, res ) => {
-    res.status(200).send("Hello World")
+    res.redirect(`/${uuidv4()}`)
+})
+
+app.get('/:room',( req, res ) => {
+    res.render('room',{ roomID })
 })
 
 server.listen(PORT,() => 
