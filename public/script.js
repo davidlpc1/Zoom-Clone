535ebe13d6c225f417bc865aecd8bac6e200f03a
Davi Lucas <davilucasdlcreator@gmail.com>
2021-01-12 21:38:18 -0300

    feat: Add the ability to view our own Video

---
+++ public/script.js
@@ -0,0 +1,25 @@
+window.addEventListener('DOMContentLoaded',() => {
+    const videoGrid = document.getElementById("video-grid")
+
+    const myVideo = document.createElement('video')
+    myVideo.muted = true
+
+    let myVideoStream
+    navigator.mediaDevices.getUserMedia({
+        video:true,
+        audio:true
+    })
+        .then(stream => {
+            myVideoStream = stream
+            addVideoStream(myVideo,stream)
+        })
+        .catch(err => alert(err.message))
+    
+    const addVideoStream = (video,stream) => {
+        video.srcObject = stream
+        video.addEventListener('loadedmetadata',() => {
+            video.play()
+        })
+        videoGrid.append(video)
+    }
+})
--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const { v4:uuidv4   } = require('uuid')
+const path = require('path')
 
 const PORT = process.env.PORT || 3030
 
 app.set('view engine','ejs')
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/',( req, res ) => {
     res.redirect(`/${uuidv4()}`)
 })
 
 app.get('/:room',( req, res ) => {
+    const roomID = req.params.room
     res.render('room',{ roomID })
 })
 
