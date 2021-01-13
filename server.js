b8181bc8ef8c1e93e084e13bda850cc7c521d5b0
Davi Lucas <davilucasdlcreator@gmail.com>
2021-01-13 19:45:58 -0300

    Everthing readygit add -A

--- server.js
+++ server.js
@@ -5,13 +5,13 @@ const app = express()
 // app.use(cors())
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
-const { ExpressPeerServer } = require('peer');
+const { ExpressPeerServer } = require('peer')
 const peerServer = ExpressPeerServer(server, {
   debug: true
-});
+})
 const { v4: uuidV4 } = require('uuid')
 
-app.use('/peerjs', peerServer);
+app.use('/peerjs', peerServer)
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
@@ -30,12 +30,11 @@ app.get('/:room',( req, res ) => {
 io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId)
-    socket.to(roomId).broadcast.emit('user-connected', userId);
-    // messages
-    socket.on('message', (message) => {
-      //send message to the same room
+    socket.to(roomId).broadcast.emit('user-connected', userId)
+
+    socket.on('message', message => {
       io.to(roomId).emit('createMessage', message)
-  }); 
+  })
 
     socket.on('disconnect', () => {
       socket.to(roomId).broadcast.emit('user-disconnected', userId)
